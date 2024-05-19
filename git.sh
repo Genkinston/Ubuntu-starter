daab76d23800288a73ebaef0cd28a69c737c4ce0
Genkinston Lurk <lurk@genkinston.ru>
2024-05-19 21:23:57 +0700

    first commit

---
+++ git.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Установка git cli
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+gh auth login
+
+#Установка github desktop
+## Direct copy-paste from official instrubtions
+## Github Desktop for Ubuntu
+## Get the @shiftkey package feed
+wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
+## Install Github Desktop for Ubuntu
+sudo apt update && sudo apt install -y github-desktop
---
+++ ubuntu_after_install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#Обновляем систему
+sudo apt update && sudo apt dist-upgrade -y
+sudo snap refresh
+#Установка пакета кодеков мультимедия ubuntu-restricted-extras
+sudo apt install ubuntu-restricted-extras  -y
+#Настраиваем контекстное меню файлового менеджера Nautilus
+apt-cache search nautilus
+sudo apt install nautilus-admin exe-thumbnailer -y
+nautilus -q
+#Устанавливаем дополнительную поддержку архиваторов
+sudo apt install p7zip-rar rar unrar unace arj cabextract -y
+#Устанавливаем программы для работы с пакетами
+sudo apt install synaptic gdebi  -y
+#Устанавливаем инструмент GNOME Tweaks для настройки рабочего окружения
+sudo apt install gnome-tweaks -y
+#Устанавливаем консольный мониторинг ресурсов Btop
+sudo apt install btop -y
+#Включаем поддержку приложений Flatpak. Подключаем репозиторий Flathub и устанавливаем Центр приложений Gnome
+sudo apt install flatpak gnome-software-plugin-flatpak gnome-software -y && flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install -y flathub com.github.tchx84.Flatseal
+#Устанавливаем расширения рабочего окружения Gnome
+sudo apt install -y chrome-gnome-shell gnome-shell-extensions gnome-shell-extension-manager
+#Поддержка принтеров и сканеров
+sudo apt install -y gcc libtool libssl-dev libc-dev libjpeg-turbo8-dev libpng-dev libtiff5-dev cups printer-driver-gutenprint
+#Установка софта для работы виртуальных машин
+sudo apt install -y virt-manager qemu-system libvirt-daemon-system qemu-utils
+sudo usermod -aG libvirt,kvm $USER
+#Установка прочего софта apt
+sudo apt install -y nala gdebi curl htop neofetch bpytop clang cargo libc6-i386 libc6-x32 libu2f-udev samba-common-bin exfat-fuse default-jdk curl wget unrar linux-headers-$(uname -r) linux-headers-generic git gstreamer1.0-vaapi corectrl rpi-imager distrobox
+#Установка прочего софта flatpak
+sudo flatpak install -y flathub com.ultimaker.cura ru.yandex.Browser org.telegram.desktop com.valvesoftware.Steam com.discordapp.Discord com.vscodium.codium org.prismlauncher.PrismLauncher org.videolan.VLC org.gnome.Boxes io.github.dvlv.boxbuddyrs
+
+#Установка docker
+./dockerinstall.sh
+
