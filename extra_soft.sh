9dbf239d7c1775695b7d21128457018e648aba17
Genkinston <lurk@genkinston.ru>
2024-06-12 14:16:48 +0700

    Update extra_soft.sh

--- extra_soft.sh
+++ extra_soft.sh
@@ -8,5 +8,7 @@ newgrp vboxusers
 #Установка прочего софта
 sudo nala install -y gdebi curl htop btop neofetch bpytop clang cargo copyq libc6-i386 libc6-x32 libu2f-udev samba-common-bin exfat-fuse default-jdk curl wget unrar linux-headers-"$(uname -r)" linux-headers-generic git gstreamer.0-vaapi corectrl rpi-imager distrobox filezilla
 
-exit
+echo -e "\n====================\nDONE\n====================\n"
+
+exit 0
 
