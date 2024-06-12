a517208ee32d70555605066306fe4001235dea66
Genkinston <lurk@genkinston.ru>
2024-06-12 16:07:22 +0700

    Update vm.sh

--- vm.sh
+++ vm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 #Установка софта для работы виртуальных машин
-sudo nala install -y virt-manager qemu-system libvirt-daemon-system qemu-utils virtualbox
-sudo usermod -aG libvirt,kvm,vboxusers "$SUDO_USER"
+nala install -y virt-manager qemu-system libvirt-daemon-system qemu-utils virtualbox
+usermod -aG libvirt,kvm,vboxusers "$SUDO_USER"
 newgrp libvirt
 newgrp kvm
 newgrp vboxusers 
