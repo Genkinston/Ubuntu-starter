8c686c3fa645c02c26cd94d910719d19a780a2e9
Genkinston <lurk@genkinston.ru>
2024-06-12 13:47:22 +0700

    Update base_soft.sh

--- base_soft.sh
+++ base_soft.sh
@@ -14,11 +14,11 @@ sudo nala install p7zip-rar rar unrar unace arj cabextract -y
 sudo nala install synnalaic gdebi  -y
 #Устанавливаем инструмент GNOME Tweaks для настройки рабочего окружения
 sudo nala install gnome-tweaks -y
-#Устанавливаем консольный мониторинг ресурсов Btop
-sudo nala install btop -y
 #Устанавливаем расширения рабочего окружения Gnome
 sudo nala install -y chrome-gnome-shell gnome-shell-extensions gnome-shell-extension-manager
 #Поддержка принтеров и сканеров
 sudo nala install -y gcc libtool libssl-dev libc-dev libjpeg-turbo8-dev libpng-dev libtiff5-dev cups printer-driver-gutenprint
 #Установка библиотек помогающий в работе python
 sudo nala install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev pkg-config make
+
+   echo -e "\n====================\nDONE\n====================\n"
