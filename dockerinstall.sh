5209b98618755b69bf5efd3a7f3ecb98cb3becc1
Genkinston <lurk@genkinston.ru>
2024-06-12 15:56:59 +0700

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -14,7 +14,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo usermod -aG docker $USER
+sudo usermod -aG docker "$SUDO_USER"
 newgrp docker
 sudo systemctl enable docker
 sudo systemctl start docker
