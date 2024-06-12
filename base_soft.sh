2a116d8adbd75e4109cdf01100261b3959a92341
Genkinston <lurk@genkinston.ru>
2024-06-12 16:51:24 +0700

    Update base_soft.sh

--- base_soft.sh
+++ base_soft.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 set -e
 #Установка пакета кодеков мультимедия ubuntu-restricted-extras
-nala install ubuntu-restricted-extras -y
+nala install -y ubuntu-restricted-extras \
 #Настраиваем контекстное меню файлового менеджера Nautilus
-apt-cache search nautilus
-nala install nautilus-admin exe-thumbnailer -y
+nala install nautilus-admin exe-thumbnailer\
 #Устанавливаем дополнительную поддержку архиваторов
-nala install p7zip-rar rar unrar unace arj cabextract -y
+p7zip-rar rar unrar unace arj cabextract \
 #Устанавливаем программы для работы с пакетами
-nala install synaptic gdebi  -y
+synaptic gdebi \
 #Устанавливаем инструмент GNOME Tweaks для настройки рабочего окружения
-nala install gnome-tweaks -y
+gnome-tweaks \
 #Устанавливаем расширения рабочего окружения Gnome
-nala install -y chrome-gnome-shell gnome-shell-extensions gnome-shell-extension-manager
+chrome-gnome-shell gnome-shell-extensions gnome-shell-extension-manager \
 #Поддержка принтеров и сканеров
-nala install -y gcc libtool libssl-dev libc-dev libjpeg-turbo8-dev libpng-dev libtiff5-dev cups printer-driver-gutenprint
+gcc libtool libssl-dev libc-dev libjpeg-turbo8-dev libpng-dev libtiff5-dev cups printer-driver-gutenprint \
 #Установка библиотек помогающий в работе python
-nala install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev pkg-config make
+build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev pkg-config make \
 
 echo -e "\n====================\nDONE\n====================\n"
 
