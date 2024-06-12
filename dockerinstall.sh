6a542c9cdc4c374d1bcf38c04dd680c3585ea60b
Genkinston <lurk@genkinston.ru>
2024-06-12 17:00:19 +0700

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -3,8 +3,8 @@ set -e
 # Add Docker's official GPG key:
 apt install nala
 nala update
-install -y ca-certificates curl
-nala install -m 0755 -d /etc/apt/keyrings
+nala install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
 
