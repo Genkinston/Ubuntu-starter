7cb9ae271b622adc7c7d7b7f73540efe3b62bc26
Genkinston <lurk@genkinston.ru>
2024-06-12 16:33:37 +0700

    Update prismlauncher.sh

--- prismlauncher.sh
+++ prismlauncher.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+nala install curl
 curl -q 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' | gpg --dearmor | tee /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg 1> /dev/null
 echo "deb [signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" | tee /etc/apt/sources.list.d/prebuilt-mpr.list
 nala update
