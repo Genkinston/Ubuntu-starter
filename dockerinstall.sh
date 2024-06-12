06a4e4b600b181d16ac41a3a2cbd817f23e33db4
Genkinston <lurk@genkinston.ru>
2024-06-12 16:56:39 +0700

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 # Add Docker's official GPG key:
-apt-get update
-apt-get install -y ca-certificates curl
-install -m 0755 -d /etc/apt/keyrings
+nala update
+nala install -y ca-certificates curl
+nala install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
 
@@ -12,13 +12,15 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+nala update
+nala install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 usermod -aG docker "$USER"
 newgrp docker
+kill -15 $PPID
 systemctl enable docker
 systemctl start docker
 systemctl restart docker
+
 
 echo -e "\n====================\nDONE\n====================\n"
 
