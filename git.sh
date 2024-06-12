86762e94b998c272ff213d56deeeff9eb3b3fd60
Genkinston <lurk@genkinston.ru>
2024-06-12 13:47:31 +0700

    Update git.sh

--- git.sh
+++ git.sh
@@ -17,3 +17,5 @@ wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee 
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
 ## Install Github Desktop for Ubuntu
 sudo apt update && sudo apt install -y github-desktop
+
+echo -e "\n====================\nDONE\n====================\n"
