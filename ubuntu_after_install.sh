ab620e5ccdf3e0cff5e789c6153113c93d9b468d
Genkinston <lurk@genkinston.ru>
2024-06-12 12:51:18 +0700

    Update ubuntu_after_install.sh

--- ubuntu_after_install.sh
+++ ubuntu_after_install.sh
@@ -39,21 +39,8 @@ while true; do
   read -r -n 1 -p "Continue or Skip? (c|s) " cs
   case $cs in
   [Cc]*)
-    #Включаем поддержку приложений Flatpak. Подключаем репозиторий Flathub и устанавливаем Центр приложений Gnome
-    sudo nala install -y flatpak gnome-software-plugin-flatpak gnome-software -y && flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    #Установка прочего софта flatpak
-    sudo flatpak install -y flathub com.ultimaker.cura 
-    sudo flatpak install -y flathub com.heroicgameslauncher.hgl
-    sudo flatpak install -y flathub ru.yandex.Browser
-    sudo flatpak install -y flathub org.telegram.desktop
-    sudo flatpak install -y flathub com.valvesoftware.Steam
-    sudo flatpak install -y flathub com.discordapp.Discord
-    sudo flatpak install -y flathub com.vscodium.codium
-    sudo flatpak install -y flathub org.prismlauncher.PrismLauncher
-    sudo flatpak install -y flathub org.videolan.VLC
-    sudo flatpak install -y flathub org.gnome.Boxes
-    sudo flatpak install -y flathub io.github.dvlv.boxbuddyrs
-    sudo flatpak install -y flathub com.github.tchx84.Flatseal
+    #Установка Flatpak и софта
+    ./flatpak.sh
     ;;
   [Ss]*)
     echo -e "\n"
