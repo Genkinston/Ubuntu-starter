e500f1fabf7a28214cc32b31f9e49df4826f8639
Genkinston <lurk@genkinston.ru>
2024-06-12 13:09:04 +0700

    Create extra_soft.sh

---
+++ extra_soft.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#Установка прочего софта
+sudo nala install -y gdebi curl htop neofetch bpytop clang cargo copyq libc6-i386 libc6-x32 libu2f-udev samba-common-bin exfat-fuse default-jdk curl wget unrar linux-headers-"$(uname -r)" linux-headers-generic git gstreamer1.0-vaapi corectrl rpi-imager distrobox filezilla
