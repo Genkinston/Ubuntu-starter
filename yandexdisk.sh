9f2983c44b91556fd6c620da39b8558131cb0e63
Genkinston <lurk@genkinston.ru>
2024-06-12 15:18:55 +0700

    Update yandexdisk.sh

--- yandexdisk.sh
+++ yandexdisk.sh
@@ -8,27 +8,25 @@ sudo apt-get install davfs2 -y
 #Добавит текущего юзера в группу доступа к сервису сетевого диска
 sudo usermod -a -G davfs2 "$USER"
 #Создаст папку куда будет монтироваться сетевой диск
-if [ ! -d /media/yandexdisk ]; then
-    sudo mkdir /media/yandexdisk
+if [ -d /media/yandexdisk ]; then
+    echo "folder already created"
 else
-    echo "folder already create"
+    sudo mkdir /media/yandexdisk
 fi
 #Установит права владения на неё текущим пользователем
 sudo chown -R "$USER":"$USER" /media/yandexdisk/
+
 #Вводим логин и пароль для Yandex disk для записи в fstab
 echo -e "Write login and password to save them in fstab for automount disk"
 read -r -p $'\n'"yandex id login: " yandexid
 read -r -p $'\n'"yandex app pass: " yandexpass
+
 #Добавить логин и пароль в папку секретов для работы davfs2
-while IFS= read -r line; do
- if [[ "$line" == *"$davfs2_secret_add_string"* ]]; then
- echo "Строка найдена в файле."
- break
- else
- echo "Строка не найдена в файле."
- sudo echo "$davfs2_secret_add_string" | sudo tee -a $davfs2_secret
- fi
-done < "$davfs2_secret"
+if [ -f $davfs2_secret ]; then
+  grep -q "$davfs2_secret_add_string" $davfs2_secret || echo "$davfs2_secret_add_string" >> $davfs2_secret
+else
+ echo "File $davfs2_secret not created"
+fi
 
 #Записываем данные в fstab для автомонтирования сетевого диска
 sudo echo "https://webdav.yandex.ru /media/yandexdisk davfs gid=$USER,uid=$USER,auto 0 0" | sudo tee -a /etc/fstab
