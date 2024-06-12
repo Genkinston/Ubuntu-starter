2b59fed2ec1a9584ecc5ddf6d54a088fb94a86b0
Genkinston <lurk@genkinston.ru>
2024-06-12 15:57:03 +0700

    Update yandexdisk.sh

--- yandexdisk.sh
+++ yandexdisk.sh
@@ -6,7 +6,7 @@ davfs2_secret_add_string="/media/yandexdisk $yandexid $yandexpass"
 #Установка необходимого пакета для работы облака как сетевого диска
 sudo apt-get install davfs2 -y
 #Добавит текущего юзера в группу доступа к сервису сетевого диска
-sudo usermod -a -G davfs2 "$USER"
+sudo usermod -a -G davfs2 "$SUDO_USER"
 #Создаст папку куда будет монтироваться сетевой диск
 if [ -d /media/yandexdisk ]; then
     echo "folder already created"
@@ -14,7 +14,7 @@ else
     sudo mkdir /media/yandexdisk
 fi
 #Установит права владения на неё текущим пользователем
-sudo chown -R "$USER":"$USER" /media/yandexdisk/
+sudo chown -R "$SUDO_USER":"$SUDO_USER" /media/yandexdisk/
 
 #Вводим логин и пароль для Yandex disk для записи в fstab
 echo
@@ -30,7 +30,7 @@ else
 fi
 
 #Записываем данные в fstab для автомонтирования сетевого диска
-sudo echo "https://webdav.yandex.ru /media/yandexdisk davfs gid=$USER,uid=$USER,auto 0 0" | sudo tee -a /etc/fstab
+sudo echo "https://webdav.yandex.ru /media/yandexdisk davfs gid=$SUDO_USER,uid=$SUDO_USER,auto 0 0" | sudo tee -a /etc/fstab
 #Монтируем диск в текущий сеанс
 sudo mount -a
 
