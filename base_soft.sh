aa6c148bd229f66fcb7c40620e885a2b96b84114
Genkinston <lurk@genkinston.ru>
2024-06-12 16:15:02 +0700

    Update base_soft.sh

--- base_soft.sh
+++ base_soft.sh
@@ -4,20 +4,20 @@ set -e
 nala install ubuntu-restricted-extras -y
 #Настраиваем контекстное меню файлового менеджера Nautilus
 apt-cache search nautilus
-nala install nautilus-admin exe-thumbnailer -y
+sudo nala install nautilus-admin exe-thumbnailer -y
 nautilus -q
 #Устанавливаем дополнительную поддержку архиваторов
-nala install p7zip-rar rar unrar unace arj cabextract -y
+sudo nala install p7zip-rar rar unrar unace arj cabextract -y
 #Устанавливаем программы для работы с пакетами
-nala install synaptic gdebi  -y
+sudo nala install synaptic gdebi  -y
 #Устанавливаем инструмент GNOME Tweaks для настройки рабочего окружения
-nala install gnome-tweaks -y
+sudo nala install gnome-tweaks -y
 #Устанавливаем расширения рабочего окружения Gnome
-nala install -y chrome-gnome-shell gnome-shell-extensions gnome-shell-extension-manager
+sudo nala install -y chrome-gnome-shell gnome-shell-extensions gnome-shell-extension-manager
 #Поддержка принтеров и сканеров
-nala install -y gcc libtool libssl-dev libc-dev libjpeg-turbo8-dev libpng-dev libtiff5-dev cups printer-driver-gutenprint
+sudo nala install -y gcc libtool libssl-dev libc-dev libjpeg-turbo8-dev libpng-dev libtiff5-dev cups printer-driver-gutenprint
 #Установка библиотек помогающий в работе python
-nala install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev pkg-config make
+sudo nala install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev pkg-config make
 
 echo -e "\n====================\nDONE\n====================\n"
 
