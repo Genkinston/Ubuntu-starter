e341685ceb80688cdca768f0bdc49499c7cbecc4
Genkinston <69857989+Genkinston@users.noreply.github.com>
2024-07-08 12:48:34 +0700

    Update extra_soft.sh

--- extra_soft.sh
+++ extra_soft.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 #Установка прочего софта
-nala install -y gdebi curl htop btop neofetch bpytop clang cargo copyq libc6-i386 libc6-x32 libu2f-udev samba-common-bin exfat-fuse default-jdk curl wget unrar linux-headers-"$(uname -r)" linux-headers-generic git gstreamer1.0-vaapi corectrl rpi-imager distrobox filezilla steam-devices
+nala install -y gdebi curl htop btop neofetch bpytop clang cargo copyq libc6-i386 libc6-x32 libu2f-udev samba-common-bin exfat-fuse default-jdk curl wget unrar linux-headers-"$(uname -r)" linux-headers-generic git gstreamer1.0-vaapi corectrl rpi-imager distrobox filezilla steam-devices lutris
 
 echo -e "\n====================\nDONE\n====================\n"
 
