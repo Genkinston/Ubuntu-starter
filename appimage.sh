8ec3e0485c1ff6897ae1b94d16af90acfcd1d1cc
Genkinston <genkinston@vk.com>
2024-07-08 16:39:13 +0700

    Update appimage.sh

--- appimage.sh
+++ appimage.sh
@@ -8,3 +8,8 @@ rm ./appimagelauncher_2.2.0-travis995.0f91801.bionic_amd64.deb
 mkdir ~/Applications
 #Установка AppImagePool
 wget https://github.com/prateekmedia/appimagepool/releases/download/5.1.0/appimagepool-5.1.0-x86_64.AppImage && mv appimagepool-5.1.0-x86_64.AppImage ~/Applications
+
+echo -e "\n====================\nDONE\n====================\n"
+
+exit 0
+
