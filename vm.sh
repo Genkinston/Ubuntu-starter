4cdd5e191b2b5c7c292aa6c80a6dd77ff26be500
Genkinston <genkinston@vk.com>
2024-07-08 16:48:40 +0700

    Update vm.sh

--- vm.sh
+++ vm.sh
@@ -3,9 +3,8 @@ set -e
 #Установка софта для работы виртуальных машин
 nala install -y virt-manager qemu-system libvirt-daemon-system qemu-utils virtualbox
 usermod -aG libvirt,kvm,vboxusers "$SUDO_USER"
-newgrp libvirt kvm vboxusers
-echo
-echo "Press ctrl + D for back after use newgrp"
+
+echo "Need reboot or use 'newgrp libvirt kvm vboxusers'"
 
 echo -e "\n====================\nDONE\n====================\n"
 
