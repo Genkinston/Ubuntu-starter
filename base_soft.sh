a92dbd650e9c08eb5d44d3ccd9a82806a297bce3
Genkinston <genkinston@vk.com>
2024-07-19 22:33:58 +0700

    Update base_soft.sh

--- base_soft.sh
+++ base_soft.sh
@@ -11,11 +11,11 @@ nala install -y synaptic gdebi
 #Устанавливаем инструмент GNOME Tweaks для настройки рабочего окружения
 nala install -y gnome-tweaks
 #Устанавливаем расширения рабочего окружения Gnome
-nala install -y chrome-gnome-shell gnome-shell-extensions gnome-shell-extension-manager \
+nala install -y chrome-gnome-shell gnome-shell-extensions gnome-shell-extension-manager
 #Поддержка принтеров и сканеров
-nala install -y gcc libtool libssl-dev libc-dev libjpeg-turbo8-dev libpng-dev libtiff5-dev cups printer-driver-gutenprint \
+nala install -y gcc libtool libssl-dev libc-dev libjpeg-turbo8-dev libpng-dev libtiff5-dev cups printer-driver-gutenprint
 #Установка библиотек помогающий в работе python
-nala install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev pkg-config make \
+nala install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev pkg-config make
 
 echo -e "\n====================\nDONE\n====================\n"
 
