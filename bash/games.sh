1e4806e04074d8d070902022fcf098c5d41672cf
Genkinston Lurk <genkinston@vk.com>
2026-01-24 10:36:59 +0700

    update to 25.10

--- bash/games.sh
+++ bash/games.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
-apt install -y curl 
-curl -q 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' | gpg --dearmor | tee /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg 1> /dev/null
-echo "deb [signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" | tee /etc/apt/sources.list.d/prebuilt-mpr.list
-
-wget https://cdn.fastly.steamstatic.com/client/installer/steam.deb
-apt install -y ./steam.deb prismlauncher lutris
-rm -f ./steam.deb
+apt install -y curl steam lutris
 
 echo -e "\n====================\nDONE\n====================\n"
 
